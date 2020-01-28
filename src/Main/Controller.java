8cf4ad245a4eeff929302281bd4d6fe03ffca16f
James <james.sharp544@protonmail.com>
2020-01-27 19:01:40 -0600

    Added ability to search itunes. Only console output

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,4 +1,42 @@
 package Main;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.http.*;
+
 public class Controller {
+    public Button searchButton;
+    public TextField searchBar;
+
+    public void search() throws IOException {
+        System.out.println("Searching... ");
+        String value = searchBar.getText();
+
+        URL url = new URL("https://itunes.apple.com/search?term=" + value + "&" + "entity=podcast");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.connect();
+
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream())
+        );
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+        System.out.println(content);
+
+    }
+
 }
