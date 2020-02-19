7d110c529c6bd166a4d0c3ab82d333c7ef88ac7e
Ally59 <izzycat242@gmail.com>
2020-02-19 15:41:28 -0600

    Working on different views

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,6 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root1 = FXMLLoader.load(getClass().getResource("Podcast.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("Search.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
