2815170c540fff7202a3784f56e3b5749ab33064
Ally59 <izzycat242@gmail.com>
2020-02-19 10:24:54 -0600

    Updating Media Player

--- src/Main/Player.java
+++ src/Main/Player.java
@@ -1,27 +1,25 @@
 package Main;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
 import javax.print.attribute.standard.MediaTray;
+import javax.swing.*;
 
 public class Player {
-    Media m;
-    MediaPlayer player;
-    MediaView v;
+    Button playbutton = new Button("Play");
+    Button back10 = new Button("Rewind 10");
+    Button forward10 = new Button("FastForward 10");
+    ProgressBar progress = new ProgressBar();
+    Media m = new Media(new JFileChooser().toString());
+   //MediaTray tray = new MediaTray();
 
-
-    public Player(String file)
+    public Player()
     {
-        m = new Media(file);
-        player = new MediaPlayer(m);
-        v = new MediaView(player);
-
 
     }
-
-
-
 
 }
