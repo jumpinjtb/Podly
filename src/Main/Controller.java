9eaf094fbcca7a8253782c7598060d1fbd832084
Ally59 <izzycat242@gmail.com>
2020-02-19 20:36:41 -0600

    we have different scenes show up in new tabs

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,4 +1,41 @@
 package Main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.beans.EventHandler;
+import java.io.IOException;
+
+
 public class Controller {
+    @FXML
+    private Button mainButton, SearchButton, PlayerButton;
+
+
+
+    public void MainButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Stage mainStage = new Stage();
+        mainStage.setScene(new Scene(root,1280, 720));
+        mainStage.show();
+    }
+
+    public void PlayerButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../Podcast/Podcast.fxml"));
+        Stage podcastStage = new Stage();
+        podcastStage.setScene(new Scene(root,1280, 720));
+        podcastStage.show();
+    }
+
+    public void SearchButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../Search/Search.fxml"));
+        Stage searchStage = new Stage();
+        searchStage.setScene(new Scene(root,1280, 720));
+        searchStage.show();
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,10 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        Parent root1 = FXMLLoader.load(getClass().getResource("../Podcast/Podcast.fxml"));
-        Parent root2 = FXMLLoader.load(getClass().getResource("../Search/Search.fxml"));
+
         primaryStage.setTitle("Podly");
-        primaryStage.setScene(new Scene(root1, 1280, 720));
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
