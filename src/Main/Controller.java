638b63390a13ed9569dda03d4f66db83bfcaf7b7
DevDogg72 <devdog2017@gmail.com>
2020-02-20 00:51:16 -0600

    Media Player implementation (not complete)

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,4 +1,28 @@
 package Main;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private MediaView mediaView;
+    private MediaPlayer mediaPlayer;
+    private Media media;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String path = new File("file.mp3").getAbsolutePath();
+        media = new Media(new File(path).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+    }
+
+
 }
