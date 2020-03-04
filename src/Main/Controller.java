931ff90458760b490b841e516f29e4cbb1e34a35
James Sharp <james.sharp544@protonmail.com>
2020-03-04 10:11:56 -0600

    Main now compiles

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,22 +1,30 @@
 package Main;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
-    private MediaView mediaView;
+    private MediaView mediaView = new MediaView();
     private MediaPlayer mediaPlayer;
     private Media media;
+    @FXML
     private Button mainButton, SearchButton, PlayerButton;
+    @FXML
     private Pane mainPane;
 
 
@@ -30,10 +38,11 @@ public class Controller implements Initializable {
     public void SearchButtonClicked(ActionEvent actionEvent) throws IOException {
         Pane searchPane = FXMLLoader.load(getClass().getResource("../Search/Search.fxml"));
         mainPane.getChildren().setAll(searchPane);
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String path = new File("MusicFile.mp3").getAbsolutePath();
+        String path = new File("src/Main/MusicFile.mp3").getAbsolutePath();
         media = new Media(new File(path).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
