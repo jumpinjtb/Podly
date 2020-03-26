c5361ba675878fff0403fe18573bf70bc30ebf7f
DevDogg72 <devdog2017@gmail.com>
2020-03-26 11:26:49 -0500

    Media Player objects moved to Podcast.java and added Volume Slider.

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -4,25 +4,15 @@ package Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
-    @FXML
-    private MediaView mediaView = new MediaView();
-    private MediaPlayer mediaPlayer;
-    private Media media;
+
+
+public class Controller{
     @FXML
     private Button mainButton, SearchButton, PlayerButton;
     @FXML
@@ -39,16 +29,5 @@ public class Controller implements Initializable {
     public void SearchButtonClicked(ActionEvent actionEvent) throws IOException {
         Pane searchPane = FXMLLoader.load(getClass().getResource("../Search/Search.fxml"));
         mainPane.getChildren().setAll(searchPane);
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        String path = new File("src/Main/MusicFile.mp3").getAbsolutePath();
-        media = new Media(new File(path).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-
-        mediaView.setMediaPlayer(mediaPlayer);
-        mediaPlayer.setAutoPlay(true);
     }
 }
