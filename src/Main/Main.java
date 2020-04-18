b76ca42db90be68ff90d8e85eda98ad61e789960
Ally59 <izzycat242@gmail.com>
2020-04-18 10:18:55 -0500

    Changed the icon on the window

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,10 +14,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Podly");
         primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(true);
-        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
         primaryStage.show();
-        primaryStage.isResizable();
+        Image icon = new Image("Main/PlayButton.png");
+        primaryStage.getIcons().add(icon);
     }
 
 
